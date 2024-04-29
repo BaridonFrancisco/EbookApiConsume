e9e98180b205a59383a446588f819e37c2bbd0d8
BaridonFrancisco <baridonfrancisco@gmail.com>
2024-04-29 11:19:43 -0300

    first commit

---
+++ src/main/java/com/franciscoBaridon/Ebook_Consumo_API/EbookConsumoApiApplication.java
@@ -0,0 +1,13 @@
+package com.franciscoBaridon.Ebook_Consumo_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbookConsumoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbookConsumoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/franciscoBaridon/Ebook_Consumo_API/EbookConsumoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franciscoBaridon.Ebook_Consumo_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbookConsumoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
